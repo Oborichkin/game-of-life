80ebe55910b992265bdcd5d45060d7ba09cf6b3a
Pavel Oborin <pavel@oborin.xyz>
2020-09-02 01:55:49 +0300

    :sparkles: Rework using sprites
    Rework using this example:
    https://arcade.academy/examples/array_backed_grid_sprites_2.html#array-backed-grid-sprites-2

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 import arcade
-import numpy as np
 
 ROWS = 25
 COLS = 25
 
-SIZE = 25
+WIDTH = HEIGHT = SIZE = 25
 MARGIN = 1
 
-WIDTH = (SIZE + MARGIN) * COLS + MARGIN
-HEIGHT = (SIZE + MARGIN) * ROWS + MARGIN
-TITLE = "Game Of Life"
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLS + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROWS + MARGIN
+SCREEN_TITLE = "Game Of Life"
 
 
 class GameOfLife(arcade.Window):
@@ -17,51 +16,42 @@ class GameOfLife(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        self.shape_list = None
-        self.grid = np.zeros((ROWS, COLS), dtype=int)
-
         arcade.set_background_color(arcade.color.BLACK)
-        self.recreate_grid()
 
-    def recreate_grid(self):
-        self.shape_list = arcade.ShapeElementList()
+        self.sprite_list = arcade.SpriteList()
+        self.grid = []
+
         for row in range(ROWS):
-            for column in range(COLS):
-                if self.grid[row][column] == 0:
-                    color = arcade.color.BLACK
-                else:
-                    color = arcade.color.GREEN
-
-                x = (MARGIN + SIZE) * column + MARGIN + SIZE // 2
-                y = (MARGIN + SIZE) * row + MARGIN + SIZE // 2
-
-                current_rect = arcade.create_rectangle_filled(x, y, SIZE, SIZE, color)
-                self.shape_list.append(current_rect)
+            self.grid.append([])
+            for col in range(COLS):
+                x = (MARGIN + WIDTH) * col + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+                sprite = arcade.SpriteSolidColor(WIDTH, HEIGHT, arcade.color.WHITE)
+                sprite.center_x, sprite.center_y = x, y
+                self.sprite_list.append(sprite)
+                self.grid[row].append(sprite)
 
     def on_draw(self):
         arcade.start_render()
-
-        self.shape_list.draw()
+        self.sprite_list.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
 
-        column = int(x // (SIZE + MARGIN))
-        row = int(y // (SIZE + MARGIN))
+        column = int(x // (MARGIN + WIDTH))
+        row = int(y // (MARGIN + HEIGHT))
 
         print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
 
         if row < ROWS and column < COLS:
 
-            if self.grid[row][column] == 0:
-                self.grid[row][column] = 1
+            if self.grid[row][column].color == arcade.color.WHITE:
+                self.grid[row][column].color = arcade.color.GREEN
             else:
-                self.grid[row][column] = 0
-        
-        self.recreate_grid()
+                self.grid[row][column].color = arcade.color.WHITE
 
 
 def main():
-    GameOfLife(WIDTH, HEIGHT, TITLE)
+    GameOfLife(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
 
 
