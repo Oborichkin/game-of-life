d23c814d1c458c8fdf2c35820306838f2bfdf202
Pavel Oborin <pavel@oborin.xyz>
2020-09-02 01:19:00 +0300

    :sparkles: Array backed grid
    Implemented array backed grid from: https://arcade.academy/examples/array_backed_grid.html#array-backed-grid
    Created class for game.

--- game.py
+++ game.py
@@ -1,28 +1,62 @@
 import arcade
 import numpy as np
 
-SCALE = 1
+ROWS = 25
+COLS = 25
 
-ROWS = 29 * 2
-COLS = 21 * 2
+SIZE = 25
+MARGIN = 1
 
-SIZE = 20 / 2
-MARGIN = 25
-PADDING = 1 / 2
+WIDTH = (SIZE + MARGIN) * COLS + MARGIN
+HEIGHT = (SIZE + MARGIN) * ROWS + MARGIN
+TITLE = "Game Of Life"
 
-world = np.zeros((ROWS, COLS), dtype=int)
 
-arcade.open_window(640, 480, "Game Of Life")
-arcade.set_background_color(arcade.color.BLACK)
-arcade.start_render()
+class MyGame(arcade.Window):
 
-for dx in range(ROWS):
-    for dy in range(COLS):
-        x = dx * (SIZE + PADDING) + MARGIN
-        y = dy * (SIZE + PADDING) + MARGIN
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
 
-        arcade.draw_rectangle_filled(x, y, SIZE, SIZE, color=arcade.color.GREEN)
+        self.grid = np.zeros((ROWS, COLS), dtype=int)
 
-arcade.finish_render()
-arcade.run()
+        arcade.set_background_color(arcade.color.BLACK)
 
+    def on_draw(self):
+        arcade.start_render()
+
+        for row in range(ROWS):
+            for column in range(COLS):
+
+                if self.grid[row][column] == 1:
+                    color = arcade.color.GREEN
+                else:
+                    color = arcade.color.BLACK
+
+                x = (MARGIN + SIZE) * column + MARGIN + SIZE // 2
+                y = (MARGIN + SIZE) * row + MARGIN + SIZE // 2
+
+                arcade.draw_rectangle_filled(x, y, SIZE, SIZE, color)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+
+        column = int(x // (SIZE + MARGIN))
+        row = int(y // (SIZE + MARGIN))
+
+        print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
+
+        if row < ROWS and column < COLS:
+
+            if self.grid[row][column] == 0:
+                self.grid[row][column] = 1
+            else:
+                self.grid[row][column] = 0
+
+
+def main():
+
+    MyGame(WIDTH, HEIGHT, TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
